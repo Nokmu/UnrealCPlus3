f86697d393fbfff0ac27e5bee94302dd7d4c738c
Nokmu <113898015+Nokmu@users.noreply.github.com>
2025-07-17 00:18:24 +0900

    Initial commit

---
+++ Source/UnrealCPlus3/UnrealCPlus3GameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealCPlus3GameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUnrealCPlus3GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUnrealCPlus3GameMode();
+};
+
+
+
